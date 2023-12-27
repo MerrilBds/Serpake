750ef9784108c2c70532763114da9a20820b366a
MerrilBds <Bdsmerril@gmail.com>
2023-12-27 22:52:12 +0100

    ajout du timer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import time
 
 class Snake:
     def __init__(self, surface):
@@ -10,44 +11,47 @@ class Snake:
         self.block = pygame.image.load("ressources/block.jpg").convert()
         self.x = 100
         self.y = 100
+        self.direction = 'down'
 
     def move_left(self):
-        # Déplace le serpent vers la gauche
-        self.x -= 10
-        self.draw()
+        # Change la direction du serpent vers la gauche
+        self.direction = 'left'
 
     def move_right(self):
-        # Déplace le serpent vers la droite
-        self.x += 10
-        self.draw()
+        # Change la direction du serpent vers la droite
+        self.direction = 'right'
 
     def move_up(self):
-        # Déplace le serpent vers le haut
-        self.y -= 10
-        self.draw()
+        # Change la direction du serpent vers le haut
+        self.direction = 'up'
 
     def move_down(self):
-        # Déplace le serpent vers le bas
-        self.y += 10
+        # Change la direction du serpent vers le bas
+        self.direction = 'down'
+
+    def walk(self):
+        # Fonction pour faire avancer le serpent dans la direction actuelle
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+        if self.direction == 'up':
+            self.y -= 10
+        if self.direction == 'down':
+            self.y += 10
+
         self.draw()
 
     def draw(self):
         # Dessine le serpent sur la surface avec sa position actuelle
-        # Effet :
-        #   Remplit la surface avec une couleur,
-        #   blit l'image du bloc à la position actuelle,
-        #   met à jour l'écran.
         self.parent_screen.fill((110, 110, 5))
         self.parent_screen.blit(self.block, (self.x, self.y))
         pygame.display.flip()
 
+
 class Game:
     def __init__(self):
         # Constructeur de la classe Game
-        # Effet :
-        #   Initialise Pygame,
-        #   crée une surface de jeu,
-        #   crée une instance de la classe Snake.
         pygame.init()
         self.surface = pygame.display.set_mode((1000, 500))
         self.snake = Snake(self.surface)
@@ -56,26 +60,32 @@ class Game:
     def run(self):
         # Boucle principale du jeu
         running = True
+
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
+
                     if event.key == K_LEFT:
                         self.snake.move_left()
+
                     if event.key == K_RIGHT:
                         self.snake.move_right()
+
                     if event.key == K_UP:
                         self.snake.move_up()
+
                     if event.key == K_DOWN:
                         self.snake.move_down()
+
                 elif event.type == QUIT:
                     running = False
 
+            self.snake.walk()
+
+            time.sleep(.2)  # Ajout d'une pause pour ralentir le déplacement du serpent
+
 if __name__ == '__main__':
-    # Fonction principale
-    # Effet :
-    #   Crée une instance de la classe Game,
-    #   lance la boucle principale du jeu.
     game = Game()
     game.run()
