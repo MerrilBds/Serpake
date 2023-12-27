2ec7c3aac853c73329c147002fa668df22875adc
MerrilBds <Bdsmerril@gmail.com>
2023-12-27 22:42:34 +0100

    convertion en orientée objet

--- main.py
+++ main.py
@@ -1,60 +1,81 @@
-import pygame #pygame documentation
+import pygame
 from pygame.locals import *
 
-def draw_block():
-    surface.fill((110, 110, 5))
-    surface.blit(block,(block_x,block_y))
-    pygame.display.flip()
+class Snake:
+    def __init__(self, surface):
+        # Constructeur de la classe Snake
+        # Paramètres :
+        #   surface : La surface sur laquelle le serpent sera dessiné.
+        self.parent_screen = surface
+        self.block = pygame.image.load("ressources/block.jpg").convert()
+        self.x = 100
+        self.y = 100
 
-if __name__ == "__main__":
-    pygame.init()
-    #  taille de la fenêtre de jeu
-    window_size = (1000, 500)
-    # Fenêtre de jeu 500 px x 500px
-    surface = pygame.display.set_mode((window_size))
+    def move_left(self):
+        # Déplace le serpent vers la gauche
+        self.x -= 10
+        self.draw()
 
-    # Rempli la surface (fenêtre) avec une couleur
-    surface.fill((110, 110, 5))
+    def move_right(self):
+        # Déplace le serpent vers la droite
+        self.x += 10
+        self.draw()
 
-    # Ressources
-    block = pygame.image.load("ressources/block.jpg").convert()
-    block_x = 100 # position  du block axe X
-    block_y = 100 # position  du block axe Y
+    def move_up(self):
+        # Déplace le serpent vers le haut
+        self.y -= 10
+        self.draw()
 
-     # Dessine le bloc sur la surface
-    surface.blit(block,(block_x,block_y))
+    def move_down(self):
+        # Déplace le serpent vers le bas
+        self.y += 10
+        self.draw()
 
-    # Mise à jour de l'écran
-    pygame.display.flip()
+    def draw(self):
+        # Dessine le serpent sur la surface avec sa position actuelle
+        # Effet :
+        #   Remplit la surface avec une couleur,
+        #   blit l'image du bloc à la position actuelle,
+        #   met à jour l'écran.
+        self.parent_screen.fill((110, 110, 5))
+        self.parent_screen.blit(self.block, (self.x, self.y))
+        pygame.display.flip()
 
-    # Initialiser la variable "running" à True pour démarrer la boucle principale
-    running = True
+class Game:
+    def __init__(self):
+        # Constructeur de la classe Game
+        # Effet :
+        #   Initialise Pygame,
+        #   crée une surface de jeu,
+        #   crée une instance de la classe Snake.
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000, 500))
+        self.snake = Snake(self.surface)
+        self.snake.draw()
 
-    # Boucle principale du jeu
-    while running:
-        # Gestion des événements Pygame
-        for event in pygame.event.get():
-            # Vérifier si l'événement est une  touche appuyée sur clavier pour rentrée dans la condition
-            if event.type == KEYDOWN:
-                # Vérifier si la touche pressée est la touche Échap (K_ESCAPE)
-                if event.key == K_ESCAPE:
-                    # Si c'est le cas, changer la valeur de "running" à False pour sortir de la boucle
+    def run(self):
+        # Boucle principale du jeu
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+                    if event.key == K_UP:
+                        self.snake.move_up()
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+                elif event.type == QUIT:
                     running = False
 
-                if event.key == K_UP:
-                    block_y -= 10
-                    draw_block()
-                if event.key == K_DOWN:
-                    block_y += 10
-                    draw_block()
-                if event.key == K_LEFT:
-                    block_x -= 10
-                    draw_block()
-                if event.key == K_RIGHT:
-                    block_x += 10
-                    draw_block()
-
-            # Vérifier si l'événement est la fermeture de la fenêtre
-            elif event.type == QUIT:
-                # Si c'est le cas, changer la valeur de "running" à False pour sortir de la boucle
-                running = False
+if __name__ == '__main__':
+    # Fonction principale
+    # Effet :
+    #   Crée une instance de la classe Game,
+    #   lance la boucle principale du jeu.
+    game = Game()
+    game.run()
